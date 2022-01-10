ae1bf3636a5a64c3f8a0c7763d68d1fac6732535
Nyllo <daniloestriga@hotmail.fr>
2022-01-10 10:35:35 +0100

    First Commit

---
+++ PASS_BUTTER_v4_obstacle.ino
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LIDARLite.h>
+
+LIDARLite myLidarLite;
+
+#define moteurD_Sens1 45
+#define moteurD_Sens2 44
+
+#define moteurG_Sens1 33
+#define moteurG_Sens2 32
+
+
+
+
+void forward(int timer){
+  digitalWrite(moteurD_Sens2, HIGH);
+  digitalWrite(moteurD_Sens1, LOW);
+  digitalWrite(moteurG_Sens2, HIGH);
+  digitalWrite(moteurG_Sens1, LOW);
+  delay(timer);
+}
+
+void backwards(int timer){
+  digitalWrite(moteurD_Sens2, LOW);
+  digitalWrite(moteurD_Sens1, HIGH);
+  digitalWrite(moteurG_Sens2, LOW);
+  digitalWrite(moteurG_Sens1, HIGH);
+  delay(timer);
+}
+
+void turnLeft(int timer){
+  digitalWrite(moteurD_Sens2, HIGH);
+  digitalWrite(moteurD_Sens1, LOW);
+  digitalWrite(moteurG_Sens2, LOW);
+  digitalWrite(moteurG_Sens1, HIGH);
+  delay(timer);
+}
+
+void turnRight(int timer){
+  digitalWrite(moteurD_Sens2, LOW);
+  digitalWrite(moteurD_Sens1, HIGH);
+  digitalWrite(moteurG_Sens2, HIGH);
+  digitalWrite(moteurG_Sens1, LOW);
+  delay(timer);
+}
+
+void stop(){
+  digitalWrite(moteurD_Sens2, LOW);
+  digitalWrite(moteurD_Sens1, LOW);
+  digitalWrite(moteurG_Sens2, LOW);
+  digitalWrite(moteurG_Sens1, LOW);
+}
+
+void LEDrun() {
+  digitalWrite(52, HIGH);
+  delay(100);
+  digitalWrite(52, LOW);
+  delay(100);
+  }
+
+void LEDstop() {
+  digitalWrite(52,HIGH);
+  delay(100);
+  digitalWrite(52,LOW);
+  delay(100);
+}
+
+void Startup(int timer){
+  digitalWrite(52, HIGH);
+  digitalWrite(moteurD_Sens2, LOW);
+  digitalWrite(moteurD_Sens1, HIGH);
+  digitalWrite(moteurG_Sens2, HIGH);
+  digitalWrite(moteurG_Sens1, LOW);
+  delay(timer);
+  digitalWrite(52, LOW);
+}
+
+
+void setup() {
+  pinMode(52, OUTPUT);
+  pinMode(moteurD_Sens1, OUTPUT);
+  pinMode(moteurD_Sens2, OUTPUT);
+  Serial.begin(9600);
+  myLidarLite.begin(0, true);
+  myLidarLite.configure(0);
+  stop();
+  delay(500); 
+}
+
+void loop() {
+  if(myLidarLite.distance() > 30  ){
+    forward(250);
+    LEDrun();
+   
+  } 
+  else {
+  //  LEDrun();
+    backwards(5);
+    stop();
+    delay(99999);
+    
+  }
+}
